7e478e415cfcf2458460a19ad9b6a84241f2f8d2
Mustafa Gamsiz <mgamsiz@netas.com.tr>
2018-03-05 14:43:47 +0300

    WebRTC version 1.

---
+++ WebRTC/src/main/java/org/gamsiz/webrtc/WebRTCCall.java
@@ -0,0 +1,11 @@
+package org.gamsiz.webrtc;
+
+import android.util.Log;
+
+public class WebRTCCall {
+
+    public void establishCall() {
+        Log.i("WebRTCCall v1", "WebRTCCall-v1 established");
+        // establishCall
+    }
+}
