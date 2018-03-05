d7e18e34ba09eec07acb5ad210ecc615146c27b7
Mustafa Gamsiz <mgamsiz@netas.com.tr>
2018-03-05 15:09:01 +0300

    WebRTC version 2.

--- WebRTC/src/main/java/org/gamsiz/webrtc/WebRTCCall.java
+++ WebRTC/src/main/java/org/gamsiz/webrtc/WebRTCCall.java
@@ -5,7 +5,12 @@ import android.util.Log;
 public class WebRTCCall {
 
     public void establishCall() {
-        Log.i("WebRTCCall v1", "WebRTCCall-v1 established");
+        Log.i("WebRTCCall v2", "WebRTCCall-v2 established");
+        // establishCall
+    }
+
+    public void endCall() {
+        Log.i("WebRTCCall v2", "WebRTCCall-v2 ended");
         // establishCall
     }
 }
